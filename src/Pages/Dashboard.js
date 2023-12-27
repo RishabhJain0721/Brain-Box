393328c31403a4ab819a673986f0fb4421452122
Rishabh <rishujain0721@gmail.com>
2023-12-28 01:14:28 +0530

    Configured firebase.js for env variables

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -40,7 +40,7 @@ const Dashboard = () => {
     } else {
       setLoading(false);
     }
-  }, []);
+  }, [currentUser.uid]);
 
   return (
     <div className="p-6 bg-gray-900 text-white min-h-screen">

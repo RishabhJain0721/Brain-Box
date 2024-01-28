3985c350ee841e53dba70bc3168e6aa6bd85389b
Sweta Singh <120044039+swetasingh8844@users.noreply.github.com>
2024-01-28 09:40:07 +0530

    Update Dashboard.js

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
-
 import { db } from "../firebase";
 import { doc, getDoc } from "firebase/firestore";
 import { AuthContext } from "../Context/AuthContext";
-
 import SubjectNav from "../Components/SubjectNav";
 import ChapterList from "../Components/ChapterList";
 
@@ -35,23 +33,24 @@ const Dashboard = () => {
       }
 
       fetchStudent();
-    } else {
+    } 
+    else {
       setLoading(false);
     }
   }, [currentUser.uid]);
 
   return (
-    <div className="p-6 bg-blue-400 text-black min-h-screen">
-      <h1 className="text-8xl max-sm:text-4xl text-center font-bold text-red-600 mb-4 w-full">
+    <div className="p-6 bg-gradient-to-r from-blue-600 to-purple-600 text-white min-h-screen">
+      <h1 className="text-4xl md:text-8xl text-center font-bold mb-4">
         Brain Box
       </h1>
 
       {loading ? (
-        <p>Loading...</p>
+        <p className="text-center">Loading...</p>
       ) : error ? (
-        <p>{error}</p>
+        <p className="text-center text-red-500">{error}</p>
       ) : (
-        <div className="flex-container">
+        <div className="flex flex-col md:flex-row md:space-x-4">
           <SubjectNav student={student} />
 
           <div className="flex-grow">
